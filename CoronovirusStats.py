71f10bfffff52297fd7e21dad9677c56fa8fa9e6
Stephen <st.98.30.03@gmail.com>
2021-07-16 18:41:22 -0400

    making a popup [still not working]

--- CoronovirusStats.py
+++ CoronovirusStats.py
@@ -6,7 +6,8 @@ from http import HTTPStatus
 import time as t
 import os
 import json
-import PyQt5 as py
+from PyQt5.QtWidgets import QMessageBox
+
 
 
 response = r.get("https://www.worldometers.info/coronavirus/")
@@ -27,12 +28,18 @@ canadaCoronovirus = response.text
 
 #Testing if coronovirus stats shown worldwide
 beautify = BeautifulSoup(coronovirusWorldWide, "html.parser")
-testString = beautify.find("div", class_="maincounter-number")
-testString = testString.text
-print("Coronovirus infected WorldWide" + testString)
+testString1 = beautify.find("div", class_="maincounter-number")
+testString1 = testString1.text
+# print("Coronovirus infected WorldWide" + testString1)
 
 #testing if canada stats are shown
 beautify = BeautifulSoup(canadaCoronovirus, "html.parser")
-testString = beautify.find("div", class_="maincounter-number")
-testString = testString.text
-print("Canada wide infected Coronovirus" + testString)
+testString2 = beautify.find("div", class_="maincounter-number")
+testString2 = testString2.text
+# print("Canada wide infected Coronovirus" + testString2)
+
+popUp = QMessageBox()
+popUp.setWindowtitle("Coronovirus Stats ")
+popUp.setTest("WorldWide Coronovirus stats: " + testString1 + "\nCanada Coronovirus Stats: " + testString2)
+
+message = popUp.exec_() # shows the message
